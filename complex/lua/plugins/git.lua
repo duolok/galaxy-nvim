165a968df6cd1476feb944094f9a00b2cee19398
dusanlecic <dusan.lecic5@gmail.com>
2023-09-05 10:02:01 +0200

    all git settings

---
+++ complex/lua/plugins/git.lua
@@ -0,0 +1,18 @@
+-- [[ GIT ]] -- 
+
+return {
+	{ "tpope/vim-fugitive",},
+	{
+		"lewis6991/gitsigns.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame = true,
+				preview_config = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
+	{ "sindrets/diffview.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+}
