98f60583febd7c99a97c9bdf41dbc9a31cc6306c
dusanlecic <dusan.lecic5@gmail.com>
2023-10-07 19:20:22 +0200

    update installation scripts

--- install_complex.sh
+++ install_complex.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm -rf .local/share/nvim/*
+rm -rf ~/.local/share/nvim/*
 rm -rf  ~/.config/nvim && mkdir -p ~/.config/nvim
 cp complex/* -rf ~/.config/nvim
--- install_fast.sh
+++ install_fast.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf .local/share/nvim/*
+rm -rf ~/.local/share/nvim/*
 rm -rf  ~/.config/nvim && mkdir -p ~/.config/nvim
 cd fast
 cp init.lua ~/.config/nvim
