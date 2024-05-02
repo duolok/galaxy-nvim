ea1dcc6c4f55c3ffb6ab1cf92a5a937aafa416b3
duolok <dusan.lecic5@gmail.com>
2024-05-02 21:25:41 +0200

    add: horizon default colorscheme

--- complex/lua/plugins/colorschemes.lua
+++ complex/lua/plugins/colorschemes.lua
@@ -1,16 +1,24 @@
 return {
 	{
-		'nyoom-engineering/oxocarbon.nvim',
+		"lunarvim/horizon.nvim",
 		lazy = false,
 		priority = 1000,
 		opts = {},
 		config = function()
-			vim.cmd.colorscheme 'oxocarbon'
+			vim.cmd.colorscheme 'horizon'
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		end,
 	},
+
 	{
 		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+		end,
+	},
+	{
+		'nyoom-engineering/oxocarbon.nvim',
 		lazy = false,
 		priority = 1000,
 		config = function()
