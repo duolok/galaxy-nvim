c7b5ca59bcda2e3be53f90b4152a67c3e4ffd718
duolok <dusan.lecic5@gmail.com>
2024-07-31 18:11:32 +0200

    add: rose pine colorscheme

--- complex/lua/plugins/colorschemes.lua
+++ complex/lua/plugins/colorschemes.lua
@@ -1,17 +1,24 @@
 return {
 	{
-		"lunarvim/horizon.nvim",
+		'rose-pine/neovim',
 		lazy = false,
 		priority = 1000,
 		opts = {},
 		config = function()
-			vim.cmd.colorscheme 'horizon'
+			vim.cmd.colorscheme 'rose-pine'
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		end,
 	},
 
 	{
 		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+		end,
+	},
+	{
+		"lunarvim/horizon.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
