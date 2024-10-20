79939d0b93622b8c3647ab222b9b4d96a59995a9
duolok <dusan.lecic5@gmail.com>
2024-10-20 16:14:13 +0200

    add: gruvbox time

--- complex/lua/plugins/colorschemes.lua
+++ complex/lua/plugins/colorschemes.lua
@@ -1,12 +1,20 @@
 return {
 	{
-		'rose-pine/neovim',
+		'morhetz/gruvbox',
 		lazy = false,
 		priority = 1000,
 		opts = {},
 		config = function()
-			vim.cmd.colorscheme 'rose-pine'
+			vim.cmd.colorscheme 'gruvbox'
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		end,
+	},
+
+	{
+		"rose-pine/neovim",
+		lazy = false,
+		priority = 1000,
+		config = function()
 		end,
 	},
 
