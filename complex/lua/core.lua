5e690ec270544271a7de2d5795bff20372a9762b
duolok <dusan.lecic5@gmail.com>
2025-07-16 22:51:34 +0200

    add: format on quit

--- complex/lua/core.lua
+++ complex/lua/core.lua
@@ -103,9 +103,23 @@ local function set_transparency()
 	vim.o.pumblend = transparency
 end
 
+local function save_on_quit()
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		callback = function()
+			local mode = vim.api.nvim_get_mode().mode
+			local filetype = vim.bo.filetype
+			if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
+				vim.cmd('lua vim.lsp.buf.format()')
+			else
+			end
+		end
+	})
+end
+
 -- Set up auto command
 local function set_autocmd() end
 
 set_keymap()
 set_transparency()
 set_autocmd()
+save_on_quit()
