045c903225df14b81a00eb09b74db6ae791af92b
duolok <dusan.lecic5@gmail.com>
2025-11-01 22:36:33 +0100

    add: the best colorscheme
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

--- complex/lua/plugins/colorschemes.lua
+++ complex/lua/plugins/colorschemes.lua
@@ -1,17 +1,19 @@
 return {
 	{
-		'morhetz/gruvbox',
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			vim.cmd.colorscheme 'gruvbox'
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		end,
+	  "vague-theme/vague.nvim",
+	  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	  priority = 1000, -- make sure to load this before all the other plugins
+	  config = function()
+		-- NOTE: you do not need to call setup if you don't want to.
+		require("vague").setup({
+		  -- optional configuration here
+		})
+		vim.cmd("colorscheme vague")
+	  end
 	},
 
 	{
-		"rose-pine/neovim",
+		'morhetz/gruvbox',
 		lazy = false,
 		priority = 1000,
 		config = function()
